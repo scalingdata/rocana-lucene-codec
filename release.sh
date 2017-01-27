49202c2daf64532409240ace5dec59b1aa96acb9
Matt Dailey <matthew.dailey1@gmail.com>
2017-01-27 16:43:10 -0500

    ROCANA-9129: publish rocana-lucene-codec to nexus repo (#12)
    * adds release.sh to perform a release

---
+++ release.sh
@@ -0,0 +1,69 @@
+# Copyright (c) 2017 Rocana
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+set -x
+
+MVN=$(type -p mvn)
+MVN_FLAGS="-B"
+
+if [ -z "${ROCANA_LUCENE_CODEC_RELEASE_VERSION}" ] || [ -z "${ROCANA_LUCENE_CODEC_DEVELOPMENT_VERSION}" ]; then
+  echo "You must set ROCANA_LUCENE_CODEC_RELEASE_VERSION and ROCANA_LUCENE_CODEC_DEVELOPMENT_VERSION before running this script."
+  exit 1
+fi
+
+if [ -n "${WORKSPACE}" ]; then
+  MVN_FLAGS="${MVN_FLAGS} -f ${WORKSPACE}/pom.xml"
+  MVN_FLAGS="${MVN_FLAGS} -Dmaven.repo.local=${WORKSPACE}/.repository"
+fi
+
+# Set the version to the next release
+${MVN} ${MVN_FLAGS} \
+  versions:set \
+    -DnewVersion=${ROCANA_LUCENE_CODEC_RELEASE_VERSION} \
+    -DgenerateBackupPoms=false \
+  || exit 1
+
+# Commit and push the version number update
+${MVN} ${MVN_FLAGS} \
+  scm:add \
+    -Dincludes=**/pom.xml \
+    -Dexcludes=**/target/**/pom.xml \
+  scm:checkin \
+    -Dmessage="ROCANA-BUILD: Preparing for release ${ROCANA_LUCENE_CODEC_RELEASE_VERSION}" \
+  || exit 1
+
+# Deploy the release to the maven repo and tag the release in git
+${MVN} ${MVN_FLAGS} \
+  -Prelease \
+  deploy \
+  scm:tag \
+    -Dtag=release-${ROCANA_LUCENE_CODEC_RELEASE_VERSION} \
+  || exit 1
+
+# Set the version to the next development version
+${MVN} ${MVN_FLAGS} \
+  versions:set \
+    -DnewVersion=${ROCANA_LUCENE_CODEC_DEVELOPMENT_VERSION} \
+    -DgenerateBackupPoms=false \
+  || exit 1
+
+# Commit and push the version number update
+${MVN} ${MVN_FLAGS} \
+  scm:add \
+    -Dincludes=**/pom.xml \
+    -Dexcludes=**/target/**/pom.xml \
+  scm:checkin \
+    -Dmessage="ROCANA-BUILD: Preparing for ${ROCANA_LUCENE_CODEC_DEVELOPMENT_VERSION} development" \
+  || exit 1
